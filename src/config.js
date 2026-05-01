4a63f2f54d31d372d1819b1db965cf67952c69a0
Tymofii Belkov <128417555+hordecandle@users.noreply.github.com>
2026-05-01 12:28:58 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -49,5 +49,5 @@ module.exports = {
   googleNativeModel: 'gemini-2.5-flash-lite', 
   fallbackModelName: 'gemini-2.5-flash-lite', 
   contextSize: 30,
-  triggerRegex: /(?<![а-яёa-z])(67|сиксевен|sixseven)(?![а-яёa-z])/i,
+  triggerRegex: /(?<![а-яёa-z])(danikai|даник|danik)(?![а-яёa-z])/i,
 };
